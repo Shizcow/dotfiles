75aa4b7df3ba82aa1a4b47569d45d25bde368330
Shizcow <pohl.devin@gmail.com>
2021-03-20 20:04:54 -0600

    My dotfiles are now dependent on my custom fork of dotter. Go figure

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -2,3 +2,4 @@
 # it changes from machine-to-machine but has a well defined format
 # I won't reasonably not have firefox installed before getting to this point
 export FF_CHROME_PATH=$(find ~/.mozilla/firefox/ -type d -name "*.default")
+
