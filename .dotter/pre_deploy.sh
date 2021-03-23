2d6c7a6eb2ca0ab2b85d34b9562e33e0c5e85dff
Shizcow <pohl.devin@gmail.com>
2021-03-23 14:24:46 -0600

    Fixed qmk keymap symlinking

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -7,4 +7,4 @@ export FF_CHROME_PATH=$(find ~/.mozilla/firefox/ -type d -name "*.default")
 git submodule update --init --recursive
 # and place symlinks to my keymap correctly
 mkdir -p qmk/firmware/keyboards/kbdfans/kbd75/keymaps/shizcow
-ln -s qmk/kbd75_keymap.c qmk/firmware/keyboards/kbdfans/kbd75/keymaps/shizcow/keymap.c
+ln -s $(pwd)/qmk/kbd75_keymap.c $(pwd)/qmk/firmware/keyboards/kbdfans/kbd75/keymaps/shizcow/keymap.c
